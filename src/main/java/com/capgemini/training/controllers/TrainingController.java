b7de1f939f1f27a6507e76ae19ad195f7caa78b5
albaCapgemini <128581180+albaCapgemini@users.noreply.github.com>
2023-04-28 09:41:28 +0200

    improve requestMapping

--- src/main/java/com/capgemini/training/controllers/TrainingController.java
+++ src/main/java/com/capgemini/training/controllers/TrainingController.java
@@ -3,15 +3,17 @@ package com.capgemini.training.controllers;
 import com.capgemini.training.services.TrainingService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping({"/hello"})
 @RequiredArgsConstructor
 public class TrainingController {
 
   private final TrainingService service;
 
-  @GetMapping(path = "/hello")
+  @GetMapping
   public String getHello() {
     return service.getHello();
   }
