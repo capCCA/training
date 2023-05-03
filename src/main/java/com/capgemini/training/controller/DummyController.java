c0ea699979e5a513664204a066b3c99b0442ab78
albertCAP <128578796+albertcapgemini@users.noreply.github.com>
2023-05-03 09:32:20 +0200

    Tarea 1

---
+++ src/main/java/com/capgemini/training/controller/DummyController.java
@@ -0,0 +1,15 @@
+package com.capgemini.training.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DummyController {
+
+  @GetMapping("hello")
+  public String Hello() {
+    return "Hello world";
+  }
+}
