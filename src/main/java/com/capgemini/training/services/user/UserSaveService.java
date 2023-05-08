7ca1ed88a2a9d2c53476023b7595a3a1c7c9a16b
albertCAP <128578796+albertcapgemini@users.noreply.github.com>
2023-05-08 10:40:37 +0200

    SaveUser - Control ID ya existente en DB

--- src/main/java/com/capgemini/training/services/user/UserSaveService.java
+++ src/main/java/com/capgemini/training/services/user/UserSaveService.java
@@ -18,26 +18,29 @@ public class UserSaveService {
 
     public ResponseEntity saveUser( Customer customer ){
 
-    //Removing spaces from customerId to save it clean in database
-    customer.setCustomerId(customer.getCustomerId().trim());
+        //Removing spaces from customerId to save it clean in database
+        customer.setCustomerId(customer.getCustomerId().trim());
 
-        //Avoiding null entitities
-        if( customer != null ){
+        //Checking that ID does not exist on database
+        if( !customerRepository.findById( customer.getCustomerId() ).isPresent() ){
+          // Avoiding null entitities
+          if (customer != null) {
 
-            if( customerRepository.save(customer) !=null ){
+            if (customerRepository.save(customer) != null) {
 
-                return ResponseEntity.ok()
-                        .body(customerMapper.customerConverterDto( customer, "El usuario " + customer + " ha sido insertado correctamente" ));
-
+              return ResponseEntity.ok()
+                  .body(
+                      customerMapper.customerConverterDto(
+                          customer, "El usuario " + customer + " ha sido insertado correctamente"));
             }
-            return ResponseEntity
-                    .status(HttpStatus.INTERNAL_SERVER_ERROR)
-                    .body("Fallo al intentar insertar el usuario " + customer );
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body("Fallo al intentar insertar el usuario " + customer);
+          }
+          return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+              .body("El objecto customer enviado es incorrecto.");
 
-        }return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body("El objecto customer enviado es incorrecto.");
-
+        }return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                    .body("El ID insertado ya se encuentra registrado en la base de datos");
 
     }
 }
