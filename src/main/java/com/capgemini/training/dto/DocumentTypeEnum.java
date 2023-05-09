bda408b82bdaddefbfb1d0d20bfe286175863753
albertCAP <128578796+albertcapgemini@users.noreply.github.com>
2023-05-09 18:29:01 +0200

    Agregado ENUM a documentType

---
+++ src/main/java/com/capgemini/training/dto/DocumentTypeEnum.java
@@ -0,0 +1,11 @@
+package com.capgemini.training.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+    @Getter
+    @AllArgsConstructor
+    public enum DocumentEnum{
+        DNI,
+        PASSPORT
+    }
